94fcdc5320e533faa975d24227609f545264912a
Jake Taufer <jwtaufer@gmail.com>
2024-04-30 12:38:13 -0600

    Added to dom-basics.js

--- week02/sandbox/dom-basics.js
+++ week02/sandbox/dom-basics.js
@@ -1,4 +1,15 @@
 const newParagraph = document.createElement("p");
 newParagraph.innerText = "Added with Javascript!";
 document.body.appendChild(newParagraph);
-setAttribute()
+
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://picsum.photos/200");
+newImage.setAttribute("alt", "Random image from picsum.");
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li><ul>";
+document.body.appendChild(newDiv);
+
+const newSection = document.createElement("section");
+newSection.innerHTML = "<h2>DOM Basics</h2><p>This was added through Javascript</p>";
+document.body.appendChild(newSection);
