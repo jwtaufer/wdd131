69f9ba793f62102a408f8100b41026a7882c44af
Jake Taufer <jwtaufer@gmail.com>
2024-05-04 23:17:39 -0600

    Did as much of Mission Statement part 2 as I could. Got stuck on '04 Write the Javascript.'

---
+++ week02/mission/mission.js
@@ -0,0 +1,12 @@
+const themeSelector = document.getElementById("Theme"); // replace with code to select dropdown element out of the HTML
+function changeTheme() {
+//check to see what the current value of our select is. The current value is conveniently found in themeSelector.value! //That's great, but where the hell is themeSelector.value?
+
+// if the value is dark then:
+// add the dark class to the body
+// change the source of the logo to point to the white logo.
+// otherwise
+// remove the dark class
+// make sure the logo src is the blue logo.
+}
+// add eventlistener to the themeSelector element here. Use the changeTheme function as the event handler function.
